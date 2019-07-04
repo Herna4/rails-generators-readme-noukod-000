0ac4dcc8847fa4b1f552e8cb3f3d0e382f315cfc
Herna Michel <labellehernamichel@yahoo.fr>
2019-07-04 02:18:21 +0000

    Automatically backed up by Learn

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe CategoriesController, type: :controller do
   # describe "GET #show" do
   #   it "returns http success" do
   #     category = Category.create(name: 'category')
-  # 
+  #
   #     get :show, id: category.id
-  # 
+  #
   #     expect(response).to have_http_status(:success)
   #   end
   # end
